cfd56a449884ebfd3314a99ed8c95d1f1d293591
Dangou DangouHaim <dangouhaim@mail.ru>
2020-11-18 04:03:25 +0300

    Splash screen

---
+++ Assets/Scripts/Source/UI/Handlers/SplashHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashHandler : MonoBehaviour
+{
+    public int HideAfter = 1;
+    
+    private static bool _init = false;
+    
+    void Awake()
+    {
+        if(_init)
+        {
+            return;
+        }
+        _init = true;
+        GetComponent<Canvas>().enabled = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Hide());
+    }
+
+    private IEnumerator Hide()
+    {
+        yield return new WaitForSeconds(HideAfter);
+        GetComponent<Canvas>().enabled = false;
+    }
+}
