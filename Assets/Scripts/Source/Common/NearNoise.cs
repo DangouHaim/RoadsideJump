050561c3b0d192f5cd1a02385c06d083d783b392
Dangou DangouHaim <dangouhaim@mail.ru>
2020-11-18 04:56:45 +0300

    Train sound

---
+++ Assets/Scripts/Source/Common/NearNoise.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearNoise : MonoBehaviour, IPoolable
+{
+    public float NoiseDistance = 5;
+    public string NoiseSound;
+
+    private bool _played = false;
+    private GameObject _player;
+
+    public void OnSpawn()
+    {
+        _played = false;
+    }
+
+    void Start()
+    {
+        _played = false;
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakeNoise();
+    }
+
+    private void MakeNoise()
+    {
+        Vector3 playerPosition = _player.transform.position;
+
+        if(Vector3.Distance(transform.position, playerPosition) <= NoiseDistance)
+        {
+            if(_played)
+            {
+                return;
+            }
+            _played = true;
+            
+            AudioManager.Instance.Play(NoiseSound);
+        }
+    }
+}
